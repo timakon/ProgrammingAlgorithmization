adf39af82347329c10435a517d615243c5d9786c
Timofey <1910tim@mail.ru>
2019-09-09 18:11:53 +0300

    lab_1.5

--- lab_1/Task_1.cs
+++ lab_1/Task_1.cs
@@ -8,39 +8,39 @@ namespace lab_1
         {
             Console.WriteLine("--------------------------------------------");
             Console.WriteLine("Номер №1");
-            Console.WriteLine("Вычислить s1 = 2 + 5 + 8 + ... + 35");
-            
-            int s1 = 2;
-            for(int i = 0; i < 35; i++)
-            {
-                s1 += 3;
-            }
-            
-            Console.WriteLine("Ответ: " + s1);
+                    Console.WriteLine("Вычислить s1 = 2 + 5 + 8 + ... + 35");
+                    
+                    int s1 = 2;
+                    for(int i = 0; i < 35; i++)
+                    {
+                        s1 += 3;
+                    }
+                    
+                    Console.WriteLine("Ответ: " + s1);
             
             Console.WriteLine("--------------------------------------------");
             Console.WriteLine("Номер №2");
-            Console.WriteLine("Вычислить s2 = 1 + 1/2 + 1/3 + 1/4 + … + 1/10");
+                    Console.WriteLine("Вычислить s2 = 1 + 1/2 + 1/3 + 1/4 + … + 1/10");
 
-            double s2 = 0;
-            for (float i = 1; i < 11; i++)
-            {
-                s2 += 1 / i;
-            }
+                    double s2 = 0;
+                    for (float i = 1; i < 11; i++)
+                    {
+                        s2 += 1 / i;
+                    }
 
-            Console.WriteLine("Ответ: " + s2);
-            
+                    Console.WriteLine("Ответ: " + s2);
+                    
             Console.WriteLine("--------------------------------------------");
             Console.WriteLine("Номер №3");
-            Console.WriteLine("Вычислить s = 2/3 + 4/5 + 6/7 + ... + 112/113");
+                    Console.WriteLine("Вычислить s = 2/3 + 4/5 + 6/7 + ... + 112/113");
 
-            double s3 = 0;
-            for (float i = 2; i < 113; i += 2)
-            {
-                s3 = i / (i + 1);
-            }
+                    double s3 = 0;
+                    for (float i = 2; i < 113; i += 2)
+                    {
+                        s3 = i / (i + 1);
+                    }
 
-            Console.WriteLine("Ответ: " + s3);
+                    Console.WriteLine("Ответ: " + s3);
             Console.WriteLine("--------------------------------------------");
         }
     }
--- lab_1/Task_2.cs
+++ lab_1/Task_2.cs
@@ -4,9 +4,57 @@ namespace lab_1
 {
     internal class Task_2
     {
-        public static void task_2(string[] arg)
+        public static void Main(string[] arg)
         {
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Номер №1");
+                    Console.WriteLine("Вычислить сумму s1 = cos x + (cos 2x)/2^2 + ... + (cos nx)/n^2 + ... .Суммирование прекратить, когда очередной член суммы по модулю будет меньше e = 0,0001");
+
+                    double x, s1 = 0;
+                    double e = 0.0001;
+                    float i1 = 1;
+                    Random rnd = new Random();
+                    x = rnd.Next(0, 360);
+                    x = x*Math.PI / 180;
+                    while ((Math.Abs((Math.Cos(i1*x))/(i1*i1))) > e)
+                    {
+                        s1 += Math.Cos(i1 * x) / (i1 * i1);
+                        i1++;
+                    }
+
+                    Console.WriteLine("Ответ: " + s1);
             
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Номер №2");
+                    Console.WriteLine("Определить наибольшее значение сомножителя n, для которого произведение р = 1 · 4 · 7 ·...· n не превышает L = 30000");
+
+                    int p2 = 1, L2 = 30000;
+                    int i2 = 1;
+                        while(p2 <= L2)
+                        {
+                            i2 += 3;
+                            p2 *= i2;
+                        }
+                        
+                     Console.WriteLine("Ответ: " + i2);
+
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Номер №3");
+                    Console.WriteLine("Определить количество членов арифметической прогрессии, сумма которых s = a + (a + h) + ... + (a + nh) не превышает заданного числа р");
+            
+                    const int h3 = 1, p3 = 20;
+                    int s3 = 0, i3 = 0, a3 = 5;
+         
+                    while (s3 < p3)
+                    {
+                        s3 += a3;
+                        i3++;
+                        a3 += h3;
+                    }
+                    Console.WriteLine("Ответ: " + i3);
+            
+            Console.WriteLine("--------------------------------------------");
+
         }
     }
 }
